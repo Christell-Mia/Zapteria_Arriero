32469eabb67621d7019e04017d04af368d6b6c11
Christell <83843563+Christell-Mia@users.noreply.github.com>
2024-08-19 14:33:14 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario-cotizacion').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario para demo
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+});
